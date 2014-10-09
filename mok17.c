422b6a07bf258b8e7b6f37a4266a4229b4d7b5b4
Jin <mok17@up.edu>
2014-10-09 01:21:41 +0000

    created mok17 function for Jin favorite food

---
+++ mok17.c
@@ -0,0 +1,9 @@
+/* this is file mok17.c */
+#include <stdio.h>
+#include "mok17.h"
+
+void mok17() {
+  
+  printf("mok17 likes steaks.\n");
+
+}
---
+++ mok17.h
@@ -0,0 +1,2 @@
+/* this is file mok17.h*/
+extern void mok17();
