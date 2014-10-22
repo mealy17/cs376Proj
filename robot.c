f6089a9ed807123f99250c437d97ebc0c3664a34
Jin <mok17@up.edu>
2014-10-22 23:13:24 +0000

    Gave the robot a facelift. Added myself to authorlist.

--- robot.c
+++ robot.c
@@ -4,7 +4,7 @@
  * A file that contains the supporting functions for main() in the
  * ~/robots example source for Software Engineering.
  *
- * @author Tanya L. Crenshaw
+ * @author Tanya L. Crenshaw and Jin Mok
  * @since August 2013
  *
  */
@@ -47,7 +47,7 @@ void robPrintMessage(int num)
 void robPrintAscii(void)
 {
   printf("    i_i    \n"
-	 "   [@_@]   \n"
+	 "   [0_0]   \n"
 	 "  /|___|\\  \n"
          "   d   b   "); 
 
